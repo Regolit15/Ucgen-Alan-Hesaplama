41e234d1e550b8ae1474e47bfe7ef6aa4615fe02
Berat G <86969477+Regolit15@users.noreply.github.com>
2022-05-27 10:59:57 +0300

    Odev

---
+++ src/angleP.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class angleP {
+    public static void main(String[] args) {
+        double  a,b,c,u,cevre,koklu;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Lutfen a Kenarini Uzunlugunu Giriniz : ");
+        a=input.nextDouble();
+        System.out.print("Lutfen b Kenarini Uzunlugunu Giriniz : ");
+        b=input.nextDouble();
+        System.out.print("Lutfen c Kenarinin Uzunlugunu Giriniz : ");
+        c=input.nextDouble();
+
+        u=(a+b+c)/2;
+        cevre=2*u;
+        System.out.println("Ucgenin Cevresi : "+ cevre);
+        koklu=u*(u-a)*(u-b)*(u-c);
+        System.out.println("Ucgenin Alani : "+Math.pow(koklu,1.0/2.0));
+
+    }
+}
